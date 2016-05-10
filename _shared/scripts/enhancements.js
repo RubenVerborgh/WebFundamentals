61cdcedd9b71ad6290965d18f2a3891d8b880e67
Ruben Verborgh <ruben@verborgh.org>
2016-05-10 21:33:17 +0200

    Disable builds by default.
    Closes #33.

--- _shared/scripts/enhancements.js
+++ _shared/scripts/enhancements.js
@@ -1,9 +1,29 @@
-// Avoid full-screen elements (including iframes) stealing focus
-setInterval(function (e) {
-  (e = document.activeElement).classList.contains('cover') && e.blur();
-}, 100);
+shower.modules.require(['shower'], function (Shower) {
+  Shower.ready(function (shower) {
+    // Disable slide builds by default, but re-enable them when 'c' is pressed
+    setBuildsEnabled(false);
+    shower.player.events.on('keydown', function (e) {
+      if (e._data.event.keyCode === 67)
+        setBuildsEnabled(true);
+    });
 
-// Advance full-slide video slides on mobile
-[].forEach.call(document.getElementsByTagName('iframe'), function (e) {
-  e.classList.contains('cover') && e.parentElement.appendChild(document.createElement('div')).classList.add('handle');
+    // Avoid full-screen elements (including iframes) stealing focus
+    setInterval(function () {
+      if (document.activeElement.classList.contains('cover'))
+        document.activeElement.blur();
+    }, 100);
+
+    // Add handles to advance video slides on mobile
+    [].forEach.call(document.getElementsByTagName('iframe'), function (e) {
+      if (e.classList.contains('cover'))
+        e.parentElement.appendChild(document.createElement('div')).classList.add('handle');
+    });
+
+    // Enables or disable slide builds
+    function setBuildsEnabled(enabled) {
+      [].forEach.call(document.getElementsByClassName('next'), function (e) {
+        e.classList[enabled ? 'remove' : 'add']('active');
+      });
+    }
+  });
 });
