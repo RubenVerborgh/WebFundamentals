ff10296f3713b587cdbdfe7464a710ab9dbbd50e
Ruben Verborgh <ruben@verborgh.org>
2018-02-13 16:09:45 +0100

    Update Shower.

--- _shared/scripts/enhancements.js
+++ _shared/scripts/enhancements.js
@@ -1,30 +1,28 @@
-shower.modules.require(['shower'], function (Shower) {
-  Shower.ready(function (shower) {
-    // Disable slide builds by default, but re-enable them when 'c' is pressed
-    setBuildsEnabled(false);
-    shower.player.events.on('keydown', function (e) {
-      if (e._data.event.keyCode === 67)
-        setBuildsEnabled(true);
-    });
-
-    // Avoid full-screen elements (including iframes) stealing focus
-    setInterval(function () {
-      var focused = document.activeElement;
-      if (focused.classList.contains('cover') || focused.classList.contains('image'))
-        focused.blur();
-    }, 100);
-
-    // Add handles to advance video slides on mobile
-    [].forEach.call(document.getElementsByTagName('iframe'), function (e) {
-      if (e.classList.contains('cover'))
-        e.parentElement.appendChild(document.createElement('div')).classList.add('handle');
-    });
-
-    // Enables or disable slide builds
-    function setBuildsEnabled(enabled) {
-      [].forEach.call(document.getElementsByClassName('next'), function (e) {
-        e.classList[enabled ? 'remove' : 'add']('active');
-      });
-    }
+(function (shower) {
+  // Disable slide builds by default, but re-enable them when 'c' is pressed
+  setBuildsEnabled(false);
+  shower.player.events.on('keydown', function (e) {
+    if (e._data.event.keyCode === 67)
+      setBuildsEnabled(true);
   });
-});
+
+  // Avoid full-screen elements (including iframes) stealing focus
+  setInterval(function () {
+    var focused = document.activeElement;
+    if (focused.classList.contains('cover') || focused.classList.contains('image'))
+      focused.blur();
+  }, 100);
+
+  // Add handles to advance video slides on mobile
+  [].forEach.call(document.getElementsByTagName('iframe'), function (e) {
+    if (e.classList.contains('cover'))
+      e.parentElement.appendChild(document.createElement('div')).classList.add('handle');
+  });
+
+  // Enables or disable slide builds
+  function setBuildsEnabled(enabled) {
+    [].forEach.call(document.getElementsByClassName('next'), function (e) {
+      e.classList[enabled ? 'remove' : 'add']('active');
+    });
+  }
+})(shower.shower);
