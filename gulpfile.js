c4a0d1e508fd941ed96941366d35dfb6085cd88b
Ruben Verborgh <ruben@verborgh.org>
2018-02-13 14:48:39 +0100

    Disable click mirroring.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp'),
     sync = require('browser-sync').create();
 
 gulp.task('default', function () {
-	sync.init({
-		notify: false,
-		server: { baseDir: './', },
-	});
-	gulp.watch(['*', '*/*', '*/*/*', '!node_modules/']).on('change', sync.reload);
+  sync.init({
+    notify: false,
+    ghostMode: false,
+    server: { baseDir: './', },
+  });
+  gulp.watch(['*', '*/*', '*/*/*', '!node_modules/']).on('change', sync.reload);
 });
